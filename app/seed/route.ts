4e8fc4f0136fa4011fa3372464c10229fdab1437
lxschutz <lxschutz@gmail.com>
2024-12-23 15:14:04 -0600

    troubleshooting db secret

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -7,8 +7,8 @@ const client = await db.connect();
 async function seedUsers() {
   await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
   await client.sql`
-     CREATE TABLE IF NOT EXISTS users (
-       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+  CREATE TABLE IF NOT EXISTS users (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
        name VARCHAR(255) NOT NULL,
        email TEXT NOT NULL UNIQUE,
        password TEXT NOT NULL
